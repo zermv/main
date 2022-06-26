b052a6d0254e17c581124d66f707fe721770f7a4
Maria Zernina <ArishchinaMV@yandex.ru>
2022-06-26 13:08:30 +0700

    Example 005 If-Else

---
+++ Example005_ConditionifElse/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+if (username == "Маша")
+{
+   Console.WriteLine("Ура! Это же МАША!"); 
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username); 
+}
